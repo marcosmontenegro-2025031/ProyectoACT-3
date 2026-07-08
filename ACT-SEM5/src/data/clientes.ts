ebbdc32615747b7a2cd70626b8f58c85700a2f79
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-07-08 07:44:47 -0600

    feat: Se creo las clases data

---
+++ ACT-SEM5/src/data/clientes.ts
@@ -0,0 +1,20 @@
+export const clientes = [
+  {
+    "id": 101,
+    "nombre": "Carlos Lopez",
+    "email": "carlos@gmail.com",
+    "activo": true
+  },
+  {
+    "id": 102,
+    "nombre": "Ana Gomez",
+    "email": "ana@hotmail.com",
+    "activo": true
+  },
+  {
+    "id": 1783518114767,
+    "nombre": "Domingo Chac",
+    "email": "domingo@gmail.com",
+    "activo": true
+  }
+];
---
+++ ACT-SEM5/src/data/productos.ts
@@ -0,0 +1,14 @@
+export const productos = [
+  {
+    "id": 1,
+    "nombre": "Laptop",
+    "precio": 8500,
+    "stock": 10
+  },
+  {
+    "id": 2,
+    "nombre": "Mouse Optico",
+    "precio": 150,
+    "stock": 25
+  }
+];
